a863104d27cc2379ed55f2812ff5b91cf62feca4
Fuad Kamal <fuad@anaara.com>
2024-05-25 15:24:17 -0400

    lesson 1 final project
    - remove starter project - there is no starter for lesosn 1
    - copy over starter project from lesson 2 as final for lesson 1

--- 01-understand-android-project-structure/Final/app/build.gradle.kts
+++ 01-understand-android-project-structure/Final/app/build.gradle.kts
@@ -1,56 +1,15 @@
-import java.io.FileInputStream
-import java.util.Properties
-
-/*
- * Copyright (c) 2023 Kodeco Inc.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
- * distribute, sublicense, create a derivative work, and/or sell copies of the
- * Software in any work that is designed, intended, or marketed for pedagogical or
- * instructional purposes related to programming, coding, application development,
- * or information technology.  Permission for such use, copying, modification,
- * merger, publication, distribution, sublicensing, creation of derivative works,
- * or sale is expressly withheld.
- *
- * This project and source code may use libraries or frameworks that are
- * released under various Open-Source licenses. Use of those libraries and
- * frameworks are governed by their own individual licenses.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 plugins {
-  id("com.android.application")
-  id("org.jetbrains.kotlin.android")
+  alias(libs.plugins.android.application)
+  alias(libs.plugins.jetbrains.kotlin.android)
 }
 
-val keysPropertiesFile: File = rootProject.file("keys.properties")
-val keysProperties = Properties()
-keysProperties.load(FileInputStream(keysPropertiesFile))
-
 android {
-  namespace = "com.kodeco.chat"
+  namespace = "com.kodeco.kodecochat"
   compileSdk = 34
 
   defaultConfig {
-    applicationId = "com.kodeco.chat"
-    minSdk = 30
+    applicationId = "com.kodeco.kodecochat"
+    minSdk = 28
     targetSdk = 34
     versionCode = 1
     versionName = "1.0"
@@ -64,31 +23,21 @@ android {
   buildTypes {
     release {
       isMinifyEnabled = false
-      proguardFiles(
-        getDefaultProguardFile("proguard-android-optimize.txt"),
-        "proguard-rules.pro"
-      )
-      buildConfigField("String", "DITTO_APP_ID", keysProperties["DITTO_APP_ID"] as String)
-      buildConfigField("String", "DITTO_TOKEN", keysProperties["DITTO_TOKEN"] as String)
-    }
-    debug {
-      buildConfigField("String", "DITTO_APP_ID", keysProperties["DITTO_APP_ID"] as String)
-      buildConfigField("String", "DITTO_TOKEN", keysProperties["DITTO_TOKEN"] as String)
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
     }
   }
   compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
   }
   kotlinOptions {
-    jvmTarget = "17"
+    jvmTarget = "1.8"
   }
   buildFeatures {
     compose = true
-    buildConfig = true
   }
   composeOptions {
-    kotlinCompilerExtensionVersion = "1.5.3"
+    kotlinCompilerExtensionVersion = "1.5.1"
   }
   packaging {
     resources {
@@ -98,37 +47,20 @@ android {
 }
 
 dependencies {
-  val composeBom = platform(libs.androidx.compose.bom)
 
-  implementation(composeBom)
-  implementation(libs.bundles.androidx)
-  implementation(libs.bundles.compose)
-
-  // compose lifecycle
-  implementation(libs.androidx.lifecycle.runtime.compose)
-
-  // used by theme
-  implementation(libs.androidx.appcompat)
-  // additional material icons
-  implementation(libs.androidx.material.icons.extended)
-
-  // SplashScreen compat library
-  implementation(libs.androidx.core.splashscreen)
-
-  // downloadable fonts
-  implementation(libs.androidx.ui.text.google.fonts)
-  //for rememberImagePainter and image loading functionality
-  implementation(libs.coil.compose)
-
-  // Date Time Library - the latest way to handle dates in Kotlin
-  implementation(libs.kotlinx.datetime)
-
-  // Ditto SDK
-  implementation(libs.ditto)
-
-  testImplementation(libs.bundles.unit.tests)
-  androidTestImplementation(composeBom)
-  androidTestImplementation(libs.bundles.instrumented.tests)
-
-  debugImplementation(libs.bundles.compose.debug)
+  implementation(libs.androidx.core.ktx)
+  implementation(libs.androidx.lifecycle.runtime.ktx)
+  implementation(libs.androidx.activity.compose)
+  implementation(platform(libs.androidx.compose.bom))
+  implementation(libs.androidx.ui)
+  implementation(libs.androidx.ui.graphics)
+  implementation(libs.androidx.ui.tooling.preview)
+  implementation(libs.androidx.material3)
+  testImplementation(libs.junit)
+  androidTestImplementation(libs.androidx.junit)
+  androidTestImplementation(libs.androidx.espresso.core)
+  androidTestImplementation(platform(libs.androidx.compose.bom))
+  androidTestImplementation(libs.androidx.ui.test.junit4)
+  debugImplementation(libs.androidx.ui.tooling)
+  debugImplementation(libs.androidx.ui.test.manifest)
 }
