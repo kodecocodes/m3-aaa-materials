95642789279a6fd8f4e91620fc74f66629d2a7d4
Fuad Kamal <fuad@anaara.com>
2024-05-25 21:33:54 -0400

    add vector asset
    Lesson 2 step: add a vector asse to the app

--- 02-use_android-resources/Final/app/src/main/java/com/kodeco/kodecochat/MainActivity.kt
+++ 02-use_android-resources/Final/app/src/main/java/com/kodeco/kodecochat/MainActivity.kt
@@ -8,11 +8,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,6 +35,18 @@ class MainActivity : ComponentActivity() {
                 .padding(50.dp)
             )
             {
+              Box() {
+                Icon(
+                  painter = painterResource(id = R.drawable.kodeco_logo_back),
+                  contentDescription = null,
+                  tint = Color(0xFFFF5A00)
+                )
+                Icon(
+                  painter = painterResource(id = R.drawable.kodeco_logo),
+                  contentDescription = null,
+                  tint = Color.White
+                )
+              }
               OutlinedTextField(
                 value = "",
                 onValueChange = {  },
