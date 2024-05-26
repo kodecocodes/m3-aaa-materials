3a498ebb03dd72a87db43e7eb6699b5337f6d4b6
Fuad Kamal <fuad@anaara.com>
2024-05-25 20:25:10 -0400

    pull from string resource
    lesson 2: pull string value from string resource file

--- 02-use_android-resources/Final/app/src/main/java/com/kodeco/kodecochat/MainActivity.kt
+++ 02-use_android-resources/Final/app/src/main/java/com/kodeco/kodecochat/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kodeco.kodecochat.ui.theme.KodecoChatTheme
@@ -35,7 +36,7 @@ class MainActivity : ComponentActivity() {
               OutlinedTextField(
                 value = "",
                 onValueChange = {  },
-                label = { Text("Label") }
+                label = { Text(text = stringResource(id = R.string.chat_entry_label)) }
               )
               Greeting(
                 name = "Android"
